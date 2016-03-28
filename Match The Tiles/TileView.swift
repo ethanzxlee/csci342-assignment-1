dfcf64a180b7bba3cfb619da6031118d6759faa4
Zhe Xian Lee <zxlee618@gmail.com>
2016-03-28 16:17:40 +1100

    Done: Part 2

---
+++ Match The Tiles/TileView.swift
@@ -0,0 +1,17 @@
+//
+//  TileView.swift
+//  Match The Tiles
+//
+//  Created by Zhe Xian Lee on 28/03/2016.
+//  Copyright © 2016 Zhe Xian Lee. All rights reserved.
+//
+
+import UIKit
+
+class TileView: UIView {
+
+//    required init?(coder aDecoder: NSCoder) {
+//        
+//    }
+
+}
